2a061fa4e461d48ff9204f9d8d7179caa5e9236d
johnnymai07 <128663973+johnnymai07@users.noreply.github.com>
2023-04-27 18:21:12 -0400

    Update State.h

--- State.h
+++ State.h
@@ -9,41 +9,63 @@ class State
 private:
 
 
-protected:
-	sf::RenderWindow* window;
-	std::map<std::string, int>* supportedKeys;
-	std::map<std::string, int> keybinds;
-	bool quit;
+// A pointer to the window object, which is used for rendering.
+    sf::RenderWindow* window;
 
-	sf::Vector2i mousePosScreen;
-	sf::Vector2i mousePosWindow;
-	sf::Vector2f mousePosView;
+    // A pointer to a map that stores supported keys and their integer values.
+    std::map<std::string, int>* supportedKeys;
 
+    // A map that stores keybinds and their corresponding integer values.
+    std::map<std::string, int> keybinds;
 
-	//Resources
-	std::vector<sf::Texture> textures;
+    // A boolean flag to indicate whether the state should quit.
+    bool quit;
 
-	//Functions
-	virtual void initializeKeybinds() = 0;
+    // A vector storing the mouse position in screen coordinates.
+    sf::Vector2i mousePosScreen;
 
+    // A vector storing the mouse position in window coordinates.
+    sf::Vector2i mousePosWindow;
+
+    // A vector storing the mouse position in view coordinates.
+    sf::Vector2f mousePosView;
+
+    // A vector storing resources, specifically textures.
+    std::vector<sf::Texture> textures;
+
+    // A pure virtual function for initializing keybinds.
+    virtual void initializeKeybinds() = 0;
 
 public:
+    // Constructor for the State class, taking a pointer to the window object and a pointer to the map of supported keys.
+    State(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys);
 
-	State(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys);
-	virtual ~State();
+    // Virtual destructor for the State class.
+    virtual ~State();
 
-	const bool& getQuit() const;
+    // A function to return the quit flag value.
+    const bool& getQuit() const;
 
+    // Pure virtual functions for managing the state:
+    
+    // A function to check for the quit condition.
+    virtual void checkForQuit();
 
-	//Pure Virtual Function: the class cant create object, but can inherit
+    // A function to end the state.
+    virtual void endState() = 0;
 
-	virtual void checkForQuit();
+    // A function to update the mouse positions.
+    virtual void updateMousePosititons();
 
-	virtual void endState() = 0;
-	virtual void updateMousePosititons();
-	virtual void updateInput(const float& dt) = 0;
-	virtual void update(const float& dt) = 0; 
-	virtual void render(sf::RenderTarget* target = NULL) = 0;
+    // A function to update input handling, taking delta time as a parameter.
+    virtual void updateInput(const float& dt) = 0;
+
+    // A function to update the state, taking delta time as a parameter.
+    virtual void update(const float& dt) = 0;
+
+    // A function to render the state, with an optional render target parameter.
+    virtual void render(sf::RenderTarget* target = NULL) = 0;
+
 
 };
 #endif
