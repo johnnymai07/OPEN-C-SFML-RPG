81f576b2a48f76f1c1de42b2541d87e421135ff3
johnnymai07 <128663973+johnnymai07@users.noreply.github.com>
2023-04-27 18:34:14 -0400

    Update GameState.h

--- GameState.h
+++ GameState.h
@@ -3,27 +3,38 @@
 
 #include "State.h"
 
+// Declaring the GameState class, which inherits from the State class.
 class GameState :
     public State
 {
 private:
+    // An Entity object representing the player.
     Entity player;
 
-    //Functions
+    // Functions:
 
+    // A function to initialize keybinds used in the game state.
     void initializeKeybinds();
 
-
 public:
+    // Constructor for the GameState class, taking a pointer to the window object and a pointer to the map of supported keys.
     GameState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys);
+
+    // Virtual destructor for the GameState class.
     virtual ~GameState();
 
-    //Functions
+    // Functions:
 
+    // A function to end the GameState.
     void endState();
 
+    // A function to update input handling, taking delta time as a parameter.
     void updateInput(const float& dt);
+
+    // A function to update the GameState, taking delta time as a parameter.
     void update(const float& dt);
+
+    // A function to render the GameState, with an optional render target parameter.
     void render(sf::RenderTarget* target = NULL);
 
 };
