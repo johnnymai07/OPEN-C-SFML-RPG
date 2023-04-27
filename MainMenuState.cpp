d3b327ce56e3798420e1eff40ff29e5a4ac3858b
johnnymai07 <128663973+johnnymai07@users.noreply.github.com>
2023-04-27 18:31:55 -0400

    Update MainMenuState.cpp

--- MainMenuState.cpp
+++ MainMenuState.cpp
@@ -1,72 +1,91 @@
 #include "MainMenuState.h"
 
+// Function to initialize the font used in the main menu.
 void MainMenuState::initializeFonts()
 {
-	if (this->font.loadFromFile("Fonts/KingsmanDemo.tttf"))
-	{
-		throw("ERROR::MAINMENUSTATE::COULD NOT LOAD FONT"); //Exit the program and prints the text
-	}
+    // Attempt to load the font from the specified file.
+    if (this->font.loadFromFile("Fonts/KingsmanDemo.tttf"))
+    {
+        // If the font fails to load, throw an error and exit the program.
+        throw("ERROR::MAINMENUSTATE::COULD NOT LOAD FONT");
+    }
 }
 
+// Function to initialize keybinds used in the main menu state.
 void MainMenuState::initializeKeybinds()
 {
+    // Opening a file stream to read keybinds from a configuration file.
+    std::ifstream ifs("Config/gamestate_keybinds.ini");
 
-	std::ifstream ifs("Config/gamestate_keybinds.ini");
+    // If the file is open and accessible.
+    if (ifs.is_open())
+    {
+        std::string key = "";
+        std::string key2 = "";
 
-	if (ifs.is_open())
-	{
-		std::string key = "";
-		std::string key2 = "";
+        // Read the keybinds from the file.
+        while (ifs >> key >> key2)
+        {
+            // Store the keybinds in the class member map.
+            this->keybinds[key] = this->supportedKeys->at(key2);
+        }
+    }
 
-		while (ifs >> key >> key2)
-		{
-			this->keybinds[key] = this->supportedKeys->at(key2);
-		}
-	}
-
-	ifs.close();
-
+    // Close the file stream.
+    ifs.close();
 }
 
+// Constructor for the MainMenuState class.
 MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys)
-	: State(window, supportedKeys)
+    : State(window, supportedKeys) // Initializing the State class with the given arguments.
 {
-	this->initializeFonts();
-	this->initializeKeybinds();
+    // Initialize the font and keybinds.
+    this->initializeFonts();
+    this->initializeKeybinds();
 
-	this->background.setSize(sf::Vector2f(window->getSize().x,window->getSize().y));
-	this->background.setFillColor(sf::Color::Blue);
+    // Set the size and color of the background.
+    this->background.setSize(sf::Vector2f(window->getSize().x,window->getSize().y));
+    this->background.setFillColor(sf::Color::Blue);
 }
 
+// Destructor for the MainMenuState class.
 MainMenuState::~MainMenuState()
 {
-
+    
 }
 
+// Function to end the MainMenuState.
 void MainMenuState::endState()
 {
-	std::cout << "Ending Game" << "\n";
+    std::cout << "Ending Game" << "\n";
 }
 
+// Function to update input handling, taking delta time as a parameter.
 void MainMenuState::updateInput(const float& dt)
 {
-	this->checkForQuit();
-
+    // Check if the quit condition is met.
+    this->checkForQuit();
 }
 
+// Function to update the MainMenuState, taking delta time as a parameter.
 void MainMenuState::update(const float& dt)
 {
-	this->updateMousePosititons();
-	this->updateInput(dt);
+    // Update the mouse positions.
+    this->updateMousePosititons();
 
+    // Update input handling.
+    this->updateInput(dt);
 }
 
+// Function to render the MainMenuState, with an optional render target parameter.
 void MainMenuState::render(sf::RenderTarget* target)
 {
-	if (!target)
+    // If the target is not specified, use the window object as the render target.
+    if (!target)
+    {
+        target = this->window;
+    }
 
-		target = this->window;
-	
-	target->draw(this->background);
+    // Draw the background on the render target.
+    target->draw(this->background);
 }
-
