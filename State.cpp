93eb2c2b3c50af48a65835c94f8964f955417d2c
johnnymai07 <128663973+johnnymai07@users.noreply.github.com>
2023-04-27 18:23:48 -0400

    Update State.cpp

--- State.cpp
+++ State.cpp
@@ -1,34 +1,50 @@
 #include "State.h"
 
+// State class constructor, taking a pointer to the window object and a pointer to the map of supported keys.
 State::State(sf::RenderWindow* window,std::map<std::string, int>* supportedKeys)
 {
-	this-> window = window;
-	this-> supportedKeys = supportedKeys;
-	this-> quit = false;
-	
+    // Assigning the window object to the class member.
+    this-> window = window;
+
+    // Assigning the map of supported keys to the class member.
+    this-> supportedKeys = supportedKeys;
+
+    // Initializing the quit flag to false.
+    this-> quit = false;
 }
 
+// State class destructor.
 State::~State()
 {
-	
+    // No resources to release.
 }
 
+// Function to return the quit flag value.
 const bool& State::getQuit() const
 {
-	return this->quit;
+    return this->quit;
 }
 
+// Function to check for the quit condition.
 void State::checkForQuit()
 {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->keybinds.at("CLOSE"))))
-	{
-		this->quit = true;
-	}
+    // Checking if the "CLOSE" key is pressed.
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->keybinds.at("CLOSE"))))
+    {
+        // If the "CLOSE" key is pressed, set the quit flag to true.
+        this->quit = true;
+    }
 }
 
+// Function to update the mouse positions.
 void State::updateMousePosititons()
 {
-	this->mousePosScreen = sf::Mouse::getPosition();
-	this->mousePosWindow = sf::Mouse::getPosition(*this->window);
-	this->mousePosView = this->window->mapPixelToCoords(sf::Mouse::getPosition(*this->window));
+    // Updating the mouse position in screen coordinates.
+    this->mousePosScreen = sf::Mouse::getPosition();
+
+    // Updating the mouse position in window coordinates.
+    this->mousePosWindow = sf::Mouse::getPosition(*this->window);
+
+    // Updating the mouse position in view coordinates, converting from window coordinates.
+    this->mousePosView = this->window->mapPixelToCoords(sf::Mouse::getPosition(*this->window));
 }
